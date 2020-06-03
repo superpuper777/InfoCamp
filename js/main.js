2c7ec29758c7e0c93277e0ac4300455d253262c2
superpuper777 <superfevrik@gmail.com>
2020-06-03 23:14:45 +0300

    Fix styles and make standarts cards dynamic

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
 'use strict';
 
 const cards = document.querySelector('.camp__standarts_cards_wr');
+let columnOne = document.querySelector('.column-1');
+let columnTwo = document.querySelector('.column-2');
 
-const columnOne = document.querySelector('.camp__standarts_cards');
-console.log(columnOne);
 
 function hideAndAppear() {
+  columnTwo.hidden = true;
+  columnOne.hidden = false
+  setTimeout(function show() {
+    columnTwo.hidden = false;
 
+    columnOne.hidden = true;
+    setTimeout(hideAndAppear, 5000)
+  }, 5000);
+}
+
+if (window.innerWidth <= 890) {
+  hideAndAppear();
+  console.log("Screen < 890")
 }
