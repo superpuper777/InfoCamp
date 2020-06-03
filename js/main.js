87bd1a4eef94d61de4ef3cb6873c13a78466f366
superpuper777 <superfevrik@gmail.com>
2020-06-03 21:57:17 +0300

    Fix html and styles and add js file

---
+++ js/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const cards = document.querySelector('.camp__standarts_cards_wr');
+
+const columnOne = document.querySelector('.camp__standarts_cards');
+console.log(columnOne);
+
+function hideAndAppear() {
+
+}
